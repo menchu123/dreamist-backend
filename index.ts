3958f1a0471da0e5590aa8ea401cb410f19b1ad9
Menchu MV <mcmvillalobos@gmail.com>
2021-11-22 21:03:51 +0100

    Initialized DB at root index

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const { initializeServer } = require("./server/index");
-
-require("dotenv").config();
+const { initializeDB } = require("./database");
 
 const port = process.env.PORT ?? process.env.SERVER_PORT ?? 5050;
 
 (async () => {
   try {
+    await initializeDB(
+      "mongodb+srv://admin:juanymedio11@cluster0.lqq22.mongodb.net/dreamist"
+    );
     initializeServer(port);
   } catch (error) {
     process.exit(1);
