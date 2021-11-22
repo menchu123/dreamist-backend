41b699d0b971610fb1787ed66934e58dad4b1136
Menchu MV <mcmvillalobos@gmail.com>
2021-11-22 21:28:01 +0100

    Added error middlewares

---
+++ server/middlewares/errors.ts
@@ -0,0 +1,19 @@
+const debug = require("debug")("dreamist:errors");
+const chalk = require("chalk");
+const { ValidationError } = require("express-validation");
+
+const notFoundErrorHandler = (req, res) => {
+  res.status(404).json({ error: "Endpoint not found" });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const errorHandler = (error, req, res, next) => {
+  debug(chalk.red("An error has occurred: ", error.message));
+  if (error instanceof ValidationError) {
+    error.code = 400;
+    error.message = "Bad romance";
+  }
+  const message = error.code ? error.message : "Idk what to tell you man";
+  res.status(error.code || 500).json({ error: message });
+};
+export = { notFoundErrorHandler, errorHandler };
