d86af53145937b590780a224085cde4d77530d9e
Menchu MV <mcmvillalobos@gmail.com>
2021-11-24 16:40:03 +0100

    Tested getUserDreams controller happy path

--- server/controllers/dreamControllers.test.ts
+++ server/controllers/dreamControllers.test.ts
@@ -1,8 +1,9 @@
-import { getDreams } from "./dreamControllers";
+import { getDreams, getUserDreams } from "./dreamControllers";
 import Dream from "../../database/models/dream";
+import User from "../../database/models/user";
 
 describe("Given a getDreams function", () => {
-  describe("When it receives an object res", () => {
+  describe("When it is called with a res and a req", () => {
     test("Then it should invoke the method json and call the Dream.find function", async () => {
       const dreams = [
         {
@@ -23,6 +24,46 @@ describe("Given a getDreams function", () => {
 
       expect(Dream.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a getUserDreams function", () => {
+  describe("When it receives an object req with a userId", () => {
+    test("Then it should invoke the method json and call the User.findById function", async () => {
+      const req = {
+        userId: 1,
+      };
+
+      const next = jest.fn();
+
+      const userDreams = {
+        id: 1,
+        name: "Elso",
+        dreams: [
+          {
+            id: 1,
+            title: "Cosis",
+          },
+          {
+            id: 2,
+            title: "wow",
+          },
+        ],
+      };
+
+      User.findById = jest
+        .fn()
+        .mockReturnValue({ populate: jest.fn().mockResolvedValue(userDreams) });
+
+      const res = {
+        json: jest.fn(),
+      };
+
+      await getUserDreams(req, res, next);
+
+      expect(User.findById).toHaveBeenCalledWith(req.userId);
+      expect(res.json).toHaveBeenCalledWith(userDreams.dreams);
     });
   });
 });
