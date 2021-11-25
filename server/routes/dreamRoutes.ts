d7b67e7148dbdfefc3e83fff1071bbddf8f504d4
Menchu MV <mcmvillalobos@gmail.com>
2021-11-25 09:31:30 +0100

    Added multer upload function

--- server/routes/dreamRoutes.ts
+++ server/routes/dreamRoutes.ts
@@ -1,15 +1,42 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
 import {
   getUserDreams,
   getUserDreamById,
   createDream,
 } from "../controllers/dreamControllers";
 import auth from "../middlewares/auth";
+import firebase from "../middlewares/firebase";
 
 const router = express.Router();
 
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "images",
+    filename: (req, file, callback) => {
+      const oldFilename = file.originalname;
+      const oldFilenameExtension = path.extname(oldFilename);
+      const oldFilenameWithoutExtension = oldFilename.replace(
+        oldFilenameExtension,
+        ""
+      );
+
+      const newFilename = `${oldFilenameWithoutExtension}-${Date.now()}-${oldFilenameExtension}`;
+      console.log(newFilename);
+      callback(null, newFilename);
+    },
+  }),
+});
+
 router.get("/user-dreams", auth, getUserDreams);
 router.get("/user-dreams/:idDream", auth, getUserDreamById);
-router.post("/user-dreams/new", auth, createDream);
+router.post(
+  "/user-dreams/new",
+  auth,
+  upload.single("image"),
+  firebase,
+  createDream
+);
 
 export default router;
