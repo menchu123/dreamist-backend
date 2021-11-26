a54f9702f1f91ffcf2dfc5c0c5d3b18435b30e25
Menchu MV <mcmvillalobos@gmail.com>
2021-11-26 14:15:23 +0100

    Added validation error test

--- server/middlewares/errors.test.ts
+++ server/middlewares/errors.test.ts
@@ -1,6 +1,7 @@
 import { notFoundErrorHandler, errorHandler } from "./errors";
 import mockResponse from "../../mocks/mockResponse";
 import mockRequest from "../../mocks/mockRequest";
+import IError from "../../interfaces/error";
 
 describe("Given an notFoundErrorHandler middleware,", () => {
   describe("When it gets a request", () => {
@@ -21,7 +22,7 @@ describe("Given an errorHandler middleware,", () => {
   describe("When it gets a request and an error and no error code", () => {
     test("Then it should send a response with a 'Idk what to tell you man' error and a status code of 500", () => {
       const res = mockResponse();
-      const error = { message: "Idk what to tell you man", code: null };
+      const error = new Error("Idk what to tell you man") as IError;
       const req = mockRequest();
       const next = () => {};
 
@@ -34,7 +35,8 @@ describe("Given an errorHandler middleware,", () => {
   describe("When it gets a request and a 'Who are you' error with a 401 error code", () => {
     test("Then it should send a response with the error's message and a status code of 401", () => {
       const res = mockResponse();
-      const error = { message: "Who are you", code: 401 };
+      const error = new Error("Who are you") as IError;
+      error.code = 401;
       const req = mockRequest();
       const next = () => {};
 
@@ -42,6 +44,23 @@ describe("Given an errorHandler middleware,", () => {
 
       expect(res.status).toHaveBeenCalledWith(401);
       expect(res.json).toHaveBeenCalledWith({ error: error.message });
+    });
+  });
+
+  describe("When it gets a request and a Validation error", () => {
+    test("Then it should send a response with a 'Bad romance' message and a status code of 400", () => {
+      const res = mockResponse();
+
+      const error = new Error("Bad romance") as IError;
+      error.statusCode = 400;
+
+      const req = mockRequest();
+      const next = () => {};
+
+      errorHandler(error, req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({ error: "Bad romance" });
     });
   });
 });
